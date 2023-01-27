7ff533811681377ea381beeb29b8f7007a431bd1
Luiz HenriqueENS <luizhenrique.egydio@gmail.com>
2023-01-27 20:04:28 -0300

    added time and location to home screen

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -1,5 +1,6 @@
 import 'package:http/http.dart';
 import 'dart:convert';
+import 'package:intl/intl.dart';
 
 class WorldTime {
   String? location; // Location name for the UI
@@ -25,7 +26,7 @@ class WorldTime {
       now = now.add(Duration(hours: int.parse(utcOffset)));
 
       // set the time property
-      time = now.toString();
+      time = DateFormat.jm().format(now);
     } catch (e) {
       print('error caught $e');
       time = 'could not get time data';
